bfe56064000dbcb8ddaa6f45489f41251e10037a
Jürgen Reichmann <j.reichmann@rdvsb.de>
2021-01-26 10:21:44 +0100

    added copyright note

--- src/commonMain/kotlin/KmFile.kt
+++ src/commonMain/kotlin/KmFile.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2021 Jürgen Reichmann, Jettingen, Germany
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
 package de.rdvsb.kmapi
 
 public expect class File(pathName: String) {
--- src/jvmMain/kotlin/KmSysJvm.kt
+++ src/jvmMain/kotlin/KmSysJvm.kt
@@ -1,8 +1,9 @@
+/*
+ * Copyright 2021 Jürgen Reichmann, Jettingen, Germany
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
 package de.rdvsb.kmapi
-
-
-
-
 
 public actual object System {
 	public actual object err {
