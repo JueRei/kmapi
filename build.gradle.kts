7bd1733e92802e49e1a5a1215509556123f9e552
Jürgen Reichmann <j.reichmann@rdvsb.de>
2021-05-28 18:14:40 +0200

    Use Kotlin 1.5.10 and recommended kotlinx library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("multiplatform") version "1.5.0"
+	kotlin("multiplatform") version "1.5.10"
 	`maven-publish`
 }
 
 group = "de.rdvsb"
 version = "0.1.3-SNAPSHOT"
 
-val coroutinesVersion = "1.4.3"
+val coroutinesVersion = "1.5.0"
 
 
 repositories {
