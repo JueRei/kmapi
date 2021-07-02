8405b41fb87955f6f933ed084e08b89faf162b03
Jürgen Reichmann <j.reichmann@rdvsb.de>
2021-07-02 20:55:13 +0200

    Kotlin 1.5.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("multiplatform") version "1.5.10"
+	kotlin("multiplatform") version "1.5.20"
 	`maven-publish`
 }
 
 group = "de.rdvsb"
 version = "0.1.3-SNAPSHOT"
 
-val kotlinVersion = "1.5.10"
+val kotlinVersion = "1.5.20"
 val coroutinesVersion = "1.5.0"
 
 
